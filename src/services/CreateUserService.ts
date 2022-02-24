f0662353e918b8c8da8e626deb92efcb5a10965f
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2022-02-24 00:28:34 -0300

    first commit

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+
+  async execute({ name, email, admin }: IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepositories);
+
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({
+      email
+    });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+    // Salvando o usuário caso ele não exista
+    const user = usersRepository.create({
+      name,
+      email,
+      admin
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+
+}
+
+export { CreateUserService }
