4ffcd5a9a23c472fad1f8ba26dffd1431d60df0a
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2022-02-24 13:03:19 -0300

    add tag controller

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController()
 
-router.post("/users", createUserController.handle)
+router.post("/users", new CreateUserController().handle)
+router.post("/tags", new CreateTagController().handle)
 
 export { router }
