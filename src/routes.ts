a042fc3358ba17d5ef3c2a4a3a4bcc2ecc57593d
Ricardo Augusto <ricardoaugusto.eb@gmail.com>
2022-03-02 23:45:00 -0300

    aula 4

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateComplimentController } from './controllers/CreateComplimentController'
 import { CreateTagController } from './controllers/CreateTagController'
 import { CreateUserController } from './controllers/CreateUserController'
 import { ensureAdmin } from './middlewares/ensureAdmin'
@@ -12,5 +13,6 @@ const router = Router()
 router.post('/tags', ensureAdmin, new CreateTagController().handle)
 router.post('/users', new CreateUserController().handle)
 router.post('/login', new AuthenticateUserController().handle)
+router.post('/compliments', new CreateComplimentController().handle)
 
 export { router }
